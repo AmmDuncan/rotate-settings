f42ee1845954953ca8210242e01161554b5ed2b7
Ammiel Yawson <ammielgyanyawson@gmail.com>
2024-03-30 14:19:30 +0000

    patch: handle unauthorized error

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,8 +1,29 @@
-import axios, { InternalAxiosRequestConfig } from "axios";
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 export const httpClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+const CANCELLED_STATUS_CODE = 499;
+function errorHandler(error: AxiosError) {
+  let { status } = error.response || {};
+
+  status = error.code === "ERR_CANCELED" ? CANCELLED_STATUS_CODE : status;
+
+  // is user is unauthorized or is using an expired token clear storage and refresh
+  if ([401, 440].includes(status!) && !window.location.pathname.includes("auth")) {
+    localStorage.clear();
+    window.location.reload();
+  }
+
+  // eslint-disable-next-line no-throw-literal
+  throw {
+    status,
+    ...(error?.response?.data || {
+      message: error.message || "Sorry, an unexpected error occurred.",
+    }),
+  };
+}
 
 // attach token to every request
 httpClient.interceptors.request.use((request: InternalAxiosRequestConfig<any>) => {
@@ -11,3 +32,5 @@ httpClient.interceptors.request.use((request: InternalAxiosRequestConfig<any>) =
 
   return request;
 });
+
+httpClient.interceptors.response.use((res) => res, errorHandler);
